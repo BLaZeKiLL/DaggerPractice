b7166f8b3467f5877c88763ce11764000f56e2c2
Devashish Lal <laldevashish@gmail.com>
2019-11-13 00:14:15 +0530

    Auth view model module created

---
+++ app/src/main/java/org/blazekill/daggerpractice/di/auth/AuthViewModelsModule.java
@@ -0,0 +1,20 @@
+package org.blazekill.daggerpractice.di.auth;
+
+import androidx.lifecycle.ViewModel;
+
+import org.blazekill.daggerpractice.di.viewmodel.ViewModelKey;
+import org.blazekill.daggerpractice.ui.auth.AuthViewModel;
+
+import dagger.Binds;
+import dagger.Module;
+import dagger.multibindings.IntoMap;
+
+@Module
+public abstract class AuthViewModelsModule {
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(AuthViewModel.class)
+    public abstract ViewModel bindAuthViewModel(AuthViewModel viewModel);
+
+}
