8de9ebd2efd207d4f5c0b8818b10d6e671727211
Devashish Lal <laldevashish@gmail.com>
2019-11-13 02:15:11 +0530

    getUser request implemented

--- app/src/main/java/org/blazekill/daggerpractice/ui/auth/AuthViewModel.java
+++ app/src/main/java/org/blazekill/daggerpractice/ui/auth/AuthViewModel.java
@@ -4,9 +4,14 @@ import android.util.Log;
 
 import androidx.lifecycle.ViewModel;
 
+import org.blazekill.daggerpractice.models.User;
 import org.blazekill.daggerpractice.network.auth.AuthApi;
 
 import javax.inject.Inject;
+
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 public class AuthViewModel extends ViewModel {
 
@@ -17,12 +22,30 @@ public class AuthViewModel extends ViewModel {
     @Inject
     public AuthViewModel(AuthApi authApi) {
         this.authApi = authApi;
-        Log.d(TAG, "AuthViewModel: view model is working");
+        authApi.getUser(1)
+            .toObservable()
+            // subscribeOn tells to run on different thread
+            .subscribeOn(Schedulers.io())
+            .subscribe(new Observer<User>() {
+                @Override
+                public void onSubscribe(Disposable d) {
+                    
+                }
 
-        if (this.authApi == null) {
-            Log.d(TAG, "AuthViewModel: auth api is NULL");
-        } else {
-            Log.d(TAG, "AuthViewModel: auth api in NOT NULL");
-        }
+                @Override
+                public void onNext(User user) {
+                    Log.d(TAG, "onNext: " + user.getEmail());
+                }
+
+                @Override
+                public void onError(Throwable e) {
+                    Log.e(TAG, "onError: ", e);
+                }
+
+                @Override
+                public void onComplete() {
+
+                }
+            });
     }
 }
