b95385202c582f5da3114b4fa9877b42e91c926e
Devashish Lal <laldevashish@gmail.com>
2019-11-13 01:52:59 +0530

    Auth Api provider created

--- app/src/main/java/org/blazekill/daggerpractice/ui/auth/AuthViewModel.java
+++ app/src/main/java/org/blazekill/daggerpractice/ui/auth/AuthViewModel.java
@@ -4,14 +4,25 @@ import android.util.Log;
 
 import androidx.lifecycle.ViewModel;
 
+import org.blazekill.daggerpractice.network.auth.AuthApi;
+
 import javax.inject.Inject;
 
 public class AuthViewModel extends ViewModel {
 
     private static final String TAG = "AuthViewModel";
 
+    private final AuthApi authApi;
+
     @Inject
-    public AuthViewModel() {
-        Log.d(TAG, "AuthViewModel: viewmodel is working");
+    public AuthViewModel(AuthApi authApi) {
+        this.authApi = authApi;
+        Log.d(TAG, "AuthViewModel: view model is working");
+
+        if (this.authApi == null) {
+            Log.d(TAG, "AuthViewModel: auth api is NULL");
+        } else {
+            Log.d(TAG, "AuthViewModel: auth api in NOT NULL");
+        }
     }
 }
