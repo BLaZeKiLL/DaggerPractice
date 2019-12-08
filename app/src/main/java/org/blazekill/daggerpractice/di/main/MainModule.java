9a0ba1ad824f176c0d8b5fe9a90cd90467bb63d4
Devashish Lal <laldevashish@gmail.com>
2019-12-08 14:37:00 +0530

    Item decoration and recycler view setup

--- app/src/main/java/org/blazekill/daggerpractice/di/main/MainModule.java
+++ app/src/main/java/org/blazekill/daggerpractice/di/main/MainModule.java
@@ -1,6 +1,7 @@
 package org.blazekill.daggerpractice.di.main;
 
 import org.blazekill.daggerpractice.network.main.MainApi;
+import org.blazekill.daggerpractice.ui.main.posts.PostsRecyclerAdapter;
 
 import dagger.Module;
 import dagger.Provides;
@@ -8,6 +9,11 @@ import retrofit2.Retrofit;
 
 @Module
 public class MainModule {
+
+    @Provides
+    static PostsRecyclerAdapter provideAdapter() {
+        return new PostsRecyclerAdapter();
+    }
 
     @Provides
     static MainApi provideMainApi(Retrofit retrofit) {
--- app/src/main/java/org/blazekill/daggerpractice/ui/main/posts/PostsFragment.java
+++ app/src/main/java/org/blazekill/daggerpractice/ui/main/posts/PostsFragment.java
@@ -9,10 +9,12 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import org.blazekill.daggerpractice.R;
 import org.blazekill.daggerpractice.di.viewmodel.ViewModelProviderFactory;
+import org.blazekill.daggerpractice.util.VerticalSpaceItemDecoration;
 
 import javax.inject.Inject;
 
@@ -24,6 +26,9 @@ public class PostsFragment extends DaggerFragment {
 
     private PostsViewModel viewModel;
     private RecyclerView recyclerView;
+
+    @Inject
+    PostsRecyclerAdapter adapter;
 
     @Inject
     ViewModelProviderFactory providerFactory;
@@ -38,6 +43,7 @@ public class PostsFragment extends DaggerFragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         recyclerView = view.findViewById(R.id.recycler_view);
         viewModel = ViewModelProviders.of(this, providerFactory).get(PostsViewModel.class);
+        initRecyclerView();
         subscribeObserver();
     }
 
@@ -45,9 +51,29 @@ public class PostsFragment extends DaggerFragment {
         viewModel.observePosts().removeObservers(getViewLifecycleOwner());
         viewModel.observePosts().observe(getViewLifecycleOwner(), listResource -> {
             if (listResource != null) {
-                Log.d(TAG, "subscribeObserver: " + listResource.data);
+                switch (listResource.status) {
+                    case LOADING: {
+                        Log.d(TAG, "subscribeObserver: LOADING...");
+                        break;
+                    }
+                    case SUCCESS: {
+                        adapter.setPosts(listResource.data);
+                        break;
+                    }
+                    case ERROR: {
+                        Log.e(TAG, "subscribeObserver: ERROR" + listResource.message);
+                        break;
+                    }
+                }
             }
         });
+    }
+
+    private void initRecyclerView() {
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        VerticalSpaceItemDecoration itemDecoration = new VerticalSpaceItemDecoration(15);
+        recyclerView.addItemDecoration(itemDecoration);
+        recyclerView.setAdapter(adapter);
     }
 
 }
